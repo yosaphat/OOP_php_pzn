30ec7cd02ebcf896bf99bb4aebd77db2fcad35da
yosaphat <yosa.ananda14@gmail.com>
2022-11-12 20:22:57 +0700

    membuat constant

---
+++ Constant.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "data/Person.php";
+
+define("makanan", "halal");
+const APP_VERSION = "0.8";
+
+echo makanan . PHP_EOL;
+
+echo APP_VERSION . PHP_EOL;
+echo Person::AUTHOR . PHP_EOL;
