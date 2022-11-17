32257536caf231b548736f332b0960d0eef466f9
yosaphat <yosa.ananda14@gmail.com>
2022-11-17 11:51:30 +0700

    interface

---
+++ data/Car.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace Data;
+
+interface Car
+
+{
+    function drive(): void;
+
+    function getTire(): int;
+}
+
+class Avanza implements Car
+{
+    public function drive(): void
+    {
+        echo "Drive Avanza" . PHP_EOL;
+    }
+
+    public function getTire(): int
+    {
+        return 4;
+    }
+}
